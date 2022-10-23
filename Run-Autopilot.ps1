29b524fa582652f56cef734dd64a3c1cd742395f
anjillan <me@Anjillan.com>
2022-10-23 00:19:02 -0400

    Create Run-Autopilot.ps1

---
+++ Run-Autopilot.ps1
@@ -0,0 +1,16 @@
+Write-Host 'Autopilot to ANJILLAN.CA' -ForegroundColor Green
+Write-Host '--------------------------' -ForegroundColor Yellow
+Write-Host 'Setting Execution Policy to Unrestricted...' -ForegroundColor Green
+$null = Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
+Write-Host 'Installing NuGet package provider...' -ForegroundColor Green
+$null = Install-PackageProvider -Name NuGet -Force
+
+Write-Host 'Installing Get-WindowsAutoPilotInfo script...' -ForegroundColor Green
+$null = Install-Script -Name Get-WindowsAutoPilotInfo -Force
+
+Write-Host 'Installing WindowsAutopilotIntune module...' -ForegroundColor Green
+$null = Install-Module -Name WindowsAutopilotIntune -Force
+
+Write-Host 'Executing "Get-WindowsAutoPilotInfo"...' -ForegroundColor Green
+Get-WindowsAutoPilotInfo -GroupTag Tag00 -Online -AddToGroup "ACS_WIN_Devices_All_Autopilot_Test_Devices" -Assign
